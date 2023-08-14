716de1f16b8c67b73a4ac3aa0176fe99822845f1
Sachindev000 <sachindev0507@gmail.com>
2023-08-14 22:49:20 +0530

    calculator commit

---
+++ main.js
@@ -0,0 +1,11 @@
+function btnClick(val){
+    document.getElementById('screen').value+=val
+}
+function btnClear(){
+    document.getElementById('screen').value=""
+}
+function equalClick(){
+    var text = document.getElementById('screen').value
+    var result=eval(text)
+    document.getElementById('screen').value=result
+}
